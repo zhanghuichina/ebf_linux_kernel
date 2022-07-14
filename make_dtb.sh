1b745ed3384e4b3de426e9c0c6a2b981f1fecead
zhanghuichina <370440020@qq.com>
2022-07-14 08:23:33 +0000

    :art: add overlay and output

--- make_dtb.sh
+++ make_dtb.sh
@@ -1,2 +1,7 @@
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- npi_v7_defconfig
 make ARCH=arm -j4 CROSS_COMPILE=arm-linux-gnueabihf- dtbs
+mkdir -p output
+rm -r output
+mkdir -p output
+cp arch/arm/boot/dts/imx6ull-mmc-npi.dtb output
+cp arch/arm/boot/dts/overlays/mt*.dtbo output
